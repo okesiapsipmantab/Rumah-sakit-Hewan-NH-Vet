a6dada6295e2b604d8519b0a7bda8c20c335aaa5
okesiapsipmantab <zaprii558@gmail.com>
2025-01-27 13:25:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript optional - for example, adding smooth scroll to anchor links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
